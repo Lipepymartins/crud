57bedd0def1a7166d991b5920225abf8715863a2
Lipepymartin <lipefenix2@gmail.com>
2023-11-05 10:03:17 -0300

    cadastrando clientes

--- crud/core/urls.py
+++ crud/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import ClienteCreateView
 
 urlpatterns = [
-    
+    path("form_cliente", ClienteCreateView.as_view()),
 ]
--- crud/core/views.py
+++ crud/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import CLiente
 
 # Create your views here.
+class ClienteCreateView(CreateView):
+    model = CLiente
+    fields = "__all__"
+    template_name = "form_cliente.html"
