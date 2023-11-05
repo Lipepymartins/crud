652a8058f00a813c94e006801925db1cf1c1b612
Lipepymartin <lipefenix2@gmail.com>
2023-11-05 12:49:27 -0300

    lista clientes

--- crud/core/urls.py
+++ crud/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ClienteCreateView
+from .views import ClienteCreateView, ClienteListView
 
 urlpatterns = [
     path("form_cliente", ClienteCreateView.as_view()),
+    path("lista_cliente", ClienteListView.as_view(),name="lista_cliente"),
 ]
--- crud/core/views.py
+++ crud/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import CLiente
 
 # Create your views here.
@@ -7,3 +7,9 @@ class ClienteCreateView(CreateView):
     model = CLiente
     fields = "__all__"
     template_name = "form_cliente.html"
+    success_url = "lista_cliente"
+
+
+class ClienteListView(ListView): #para listas
+    model = CLiente
+    template_name = "lista_cliente.html"
